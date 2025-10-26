ba247f83e4077eaba89e93ebd9c767e23e060cce
ibobro <ibragim.z@iman.uz>
2025-10-26 21:09:56 +0500

    feat: add AboutHero component with introductory content for Stemora

---
+++ app/components/about/hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="hero" class="flex h-dvh items-center justify-center">
+    <div class="container grid grid-cols-1 gap-6 lg:grid-cols-2">
+      <div class="order-2 space-y-5 text-center lg:order-1 lg:text-start">
+        <p class="text-blue text-[42px] leading-[130%] font-bold tracking-[5%] lg:text-[62px]">About Stemora</p>
+        <h1 class="text-blue text-[20px] leading-[130%] font-bold tracking-[5%] lg:text-[32px]">
+          Stemora is a professional couch and upholstery cleaning company based in New York City.
+        </h1>
+
+        <p class="text-[14px] leading-[120%] font-medium tracking-[0%] lg:text-[18px]">
+          We provide trusted services built on modern steam technology, eco-friendly solutions, and a customer-first
+          approach. Our goal is to help New Yorkers maintain clean, fresh, and healthy spaces — whether at home or in
+          the office.
+        </p>
+      </div>
+
+      <img alt="sofa" src="~/assets/images/work.png" class="order-1 h-auto w-full object-contain lg:order-2" />
+    </div>
+  </section>
+</template>
