1d656a9a4d170d095985b5e6856c9566abf1c97a
ibobro <ibragim.z@iman.uz>
2025-11-11 14:21:23 +0500

    feat: remove booking link from cards and add booking iframe to index page

--- app/pages/book-now/index.vue
+++ app/pages/book-now/index.vue
@@ -2,4 +2,10 @@
   <BookHero />
   <BookCards />
   <BookPlease />
+  <section class="container">
+    <iframe
+      class="h-[calc(100dvh-50dvh)] w-full"
+      src="https://app.squareup.com/appointments/book/2ks607myr9m1j3/L920KXWQGGGAG/start"
+    ></iframe>
+  </section>
 </template>
