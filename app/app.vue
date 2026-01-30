1c346555ac17875633f4c559fb5118eb98029d28
Dumster <74446521+dumsterB@users.noreply.github.com>
2026-01-30 10:24:32 +0500

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,22 @@
+<script setup>
+useHead({
+  script: [
+    {
+      async: true,
+      src: 'https://www.googletagmanager.com/gtag/js?id=AW-17884624122',
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-17884624122');
+      `,
+    },
+  ],
+})
+</script>
+
 <template>
   <div class="@container/main">
     <BaseNav />
